c314cb4deecb89b7404bc3ea2d7a06d590d38235
jared brown <jaredbrown1@hotmail.com>
2023-04-26 20:05:51 -0700

    categories showing in Airbnb your home section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
 import { Nunito } from 'next/font/google';
 
 import './globals.css';
+
 import Navbar from './components/navbar/navbar';
 import RegisterModal from './components/modals/RegisterModal';
 import LoginModal from './components/modals/LoginModal';
+import RentModal from './components/modals/RentModal';
+
 import ToasterProvider from './providers/ToasterProvider';
 import getCurrentUser from './actions/getCurrentUser';
 
@@ -27,6 +30,7 @@ export default async function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ToasterProvider />
+        <RentModal />
         <LoginModal />
         <RegisterModal />
         <Navbar currentUser={currentUser} />
