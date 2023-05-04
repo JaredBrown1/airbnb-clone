3483682514c5015936cb7597f5066222e71be9d6
Jared Brown <jaredbrown1@hotmail.com>
2023-05-03 21:29:46 -0700

    reservations showing correctly according to user logged in

--- app/trips/page.tsx
+++ app/trips/page.tsx
@@ -1,35 +1,45 @@
-import EmptyState from "../components/EmptyState";
+import EmptyState from "@/app/components/EmptyState";
+
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import getReservations from "@/app/actions/getReservations";
+
 import TripsClient from "./TripsClient";
 
-import getCurrentUser from "../actions/getCurrentUser";
-import getReservations from "../actions/getReservations";
-
 const TripsPage = async () => {
-    const currentUser = await getCurrentUser()
+  const currentUser = await getCurrentUser();
 
-    if (!currentUser) {
-        return (
-            <EmptyState
-                title="Unauthorized"
-                subtitle="Please login"
-            />
-        )
-    }
-
-    const reservations = await getReservations({
-        userId: currentUser.id
-    })
-
-    if (reservations.length === 0) {
-        <EmptyState title="No trips found" subtitle="Looks like you havent reserved any trips" />
-    }
-
+  if (!currentUser) {
     return (
-        <TripsClient
+      
+        <EmptyState
+          title="Unauthorized"
+          subtitle="Please login"
+        />
+      
+    );
+  }
+
+  const reservations = await getReservations({ userId: currentUser.id });
+
+  if (reservations.length === 0) {
+    return (
+      
+        <EmptyState
+          title="No trips found"
+          subtitle="Looks like you havent reserved any trips."
+        />
+      
+    );
+  }
+
+  return (
+   
+      <TripsClient
         reservations={reservations}
         currentUser={currentUser}
-        />
-    )
+      />
+   
+  );
 }
-
+ 
 export default TripsPage;
