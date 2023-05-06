e8dac68fbe3205133d83d47eea3846c8cbea6915
Jared Brown <jaredbrown1@hotmail.com>
2023-05-06 13:37:44 -0700

    error fixed

--- app/favorites/FavoritesClient.tsx
+++ app/favorites/FavoritesClient.tsx
@@ -16,7 +16,7 @@ const FavoritesClient: React.FC<FavoritesClientProps> = ({
         <Container>
             <Heading title="Favorites" subtitle="List of places you have favorited" />
             <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
-                {listings.map((listing) => (
+                {listings.map((listing: any) => (
                     <ListingCard currentUser={currentUser} key={listing.id} data={listing} />
                 ))}
             </div>
